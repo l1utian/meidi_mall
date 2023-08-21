5ebff072f1d9be92c59ea25f47124a604c1e45ad
Aaron Su <402090261@qq.com>
2023-08-21 21:20:51 +0800

    更新login

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,7 +2,7 @@ import { get, post } from "@/utils/request";
 
 //登录
 export const login = (params): Promise<any> => {
-  return post("/login", params, true);
+  return post("/login", params);
 };
 
 //保存用户信息
